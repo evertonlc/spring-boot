1faca35c35b2dfe9e6fd958b1422cf7a76f3530c
Éverton Luis Confortin <evertonconfortin@gmail.com>
2017-08-15 12:52:40 -0300

    - Adding Bean Validation to Categoria entity; - Adding default field validation message mapping;

--- src/main/java/com/algaworks/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
+++ src/main/java/com/algaworks/algamoney/api/exceptionhandler/AlgamoneyExceptionHandler.java
@@ -7,40 +7,66 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @ControllerAdvice
 public class AlgamoneyExceptionHandler extends ResponseEntityExceptionHandler {
 
-	@Autowired
-	private MessageSource messageSource;
-	
-	@Override
-	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
-			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		
-		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
-		String mensagemDesenvolvedor = ex.getCause().getMessage();
-		return handleExceptionInternal(ex, new Erro(mensagemUsuario, mensagemDesenvolvedor), headers, status, request);
-	}
-	
-	public static class Erro {
-		private String mensagemUsuario;
-		private String mensagemDesenvolvedor;
-		public Erro(String mensagemUsuario, String mensagemDesenvolvedor) {
-			super();
-			this.mensagemUsuario = mensagemUsuario;
-			this.mensagemDesenvolvedor = mensagemDesenvolvedor;
-		}
-		public String getMensagemUsuario() {
-			return mensagemUsuario;
-		}
-		public String getMensagemDesenvolvedor() {
-			return mensagemDesenvolvedor;
-		}
-		
-	}
+    @Autowired
+    private MessageSource messageSource;
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
+        String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
+        String mensagemDesenvolvedor = ex.getCause().getMessage();
+        List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+        return handleExceptionInternal(ex, erros, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<Erro> erros = criarListaDeErros(ex.getBindingResult());
+        return super.handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    private List<Erro> criarListaDeErros(BindingResult bindResult) {
+        ArrayList<Erro> erros = new ArrayList<>();
+
+        for (FieldError fieldError : bindResult.getFieldErrors()) {
+            erros.add(new Erro(messageSource.getMessage(fieldError, LocaleContextHolder.getLocale()), fieldError.toString()));
+        }
+
+        return erros;
+    }
+
+    public static class Erro {
+        private String mensagemUsuario;
+        private String mensagemDesenvolvedor;
+
+        public Erro(String mensagemUsuario, String mensagemDesenvolvedor) {
+            super();
+            this.mensagemUsuario = mensagemUsuario;
+            this.mensagemDesenvolvedor = mensagemDesenvolvedor;
+        }
+
+        public String getMensagemUsuario() {
+            return mensagemUsuario;
+        }
+
+        public String getMensagemDesenvolvedor() {
+            return mensagemDesenvolvedor;
+        }
+
+    }
 }
 
